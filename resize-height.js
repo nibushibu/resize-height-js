c37622a5fa9ebc7cd20a1e9a74c0975649ad3fc8
nibushibu <nibushibu@gmail.com>
2013-03-28 19:17:33 +0900

    初校

---
+++ resize-height.js
@@ -0,0 +1,37 @@
+/*
+ * resizeHeight - jQuery Plugin
+ *
+ * Author Genki Katsutani
+ * http://nibushibu.com/
+ * 
+ * Licensed under the MIT License
+ */
+
+ (function($){
+
+  function resizeHeight(){
+    var el = $('.js-resize-height');
+    var obj = [];
+    var keyArr = [];
+    el.height('auto');
+    for (var i = el.length - 1; i >= 0; i--) {
+      var key = $('.js-resize-height:eq('+i+')').data('resize-group');
+      var val = $('.js-resize-height:eq('+i+')').height();
+      if(!obj[key]){
+        obj[key] = [];
+        keyArr.push(key);
+      }
+      obj[key].push(val);
+    };
+    for (var i=keyArr.length-1; i>=0; i--) {
+      var maxHeight = Math.max.apply(null, obj[keyArr[i]]);
+      console.log(maxHeight);
+      $('.js-resize-height[data-resize-group="'+keyArr[i]+'"]').height(maxHeight);
+    };
+  }
+
+ })(jQuery);
+
+jQuery(document).ready(function(){
+  resizeHeight();
+});
