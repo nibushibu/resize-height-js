1ba4227bb8bb4d8de20b3a7aa178eacc5fad0839
Genki Katsutani <mm@nibushibu.com>
2015-12-31 14:49:12 +0900

    Update resize-height.js

--- resize-height.js
+++ resize-height.js
@@ -8,24 +8,45 @@
  */
 
 function resizeHeight(){
+
   var el = $('.js-resize-height');
-  var obj = [];
-  var keyArr = [];
-  el.height('auto');
-  for (var i = el.length - 1; i >= 0; i--) {
-    var key = $('.js-resize-height:eq(' + i + ')').data('resize-group');
-    var val = $('.js-resize-height:eq(' + i + ')').height();
-    if(!obj[key]){
-      obj[key] = [];
-      keyArr.push(key);
+  var timer = false;
+
+  $(window).resize(function() {
+
+    el.height('auto');
+
+    if (timer !== false) {
+      clearTimeout(timer);
     }
-    obj[key].push(val);
-  };
-  for (var i = keyArr.length - 1; i >= 0; i--) {
-    var maxHeight = Math.max.apply(null, obj[keyArr[i]]);
-    console.log(maxHeight);
-    $('.js-resize-height[data-resize-group="' + keyArr[i] + '"]').height(maxHeight);
-  };
+
+    timer = setTimeout(function() {
+      resize();
+    }, 200);
+  });
+
+  function resize(){
+
+    var obj = [];
+    var keyArr = [];
+
+    for (var i = el.length - 1; i >= 0; i--) {
+      var key = $('.js-resize-height:eq(' + i + ')').data('resize-group');
+      var val = $('.js-resize-height:eq(' + i + ')').height();
+      if(!obj[key]){
+        obj[key] = [];
+        keyArr.push(key);
+      }
+      obj[key].push(val);
+    };
+    for (var i = keyArr.length - 1; i >= 0; i--) {
+      var maxHeight = Math.max.apply(null, obj[keyArr[i]]);
+      // console.log(maxHeight);
+      $('.js-resize-height[data-resize-group="' + keyArr[i] + '"]').height(maxHeight);
+    };
+  }
+
+  resize();
 }
 
 $(function(){
